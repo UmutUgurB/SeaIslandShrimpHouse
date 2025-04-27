d41c01db7ec71768acae1331c5e2811df90e31fc
UmutUgurB <75566895+UmutUgurB@users.noreply.github.com>
2025-04-27 23:58:17 +0300

    discount controller

---
+++ SeaIsland.WebApi/Controllers/ContactsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SeaIsland.BusinessLayer.Abstract;
+using SeaIsland.DtoLayer.ContactDto;
+using SeaIsland.EntityLayer.Entities;
+
+namespace SeaIsland.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController(IContactService _contactService,IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetContact()
+        {
+            var values = _contactService.TGetlistAll();
+            return Ok(values);  
+        }
+        [HttpPost]
+        public IActionResult PostContact(CreateContactDto createContactDto) 
+        {
+            var newValue = _mapper.Map<Contact>(createContactDto);
+            _contactService.TAdd(newValue);
+            return Ok("Yeni İletişim Bloğu eklendi");
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id) 
+        {
+            var value = _contactService.TGetById(id);
+            return Ok(value);
+        
+        }
+        [HttpDelete]
+        public IActionResult DeleteById(int id) 
+        {
+            var value = _contactService.TGetById(id);
+            _contactService.TDelete(value);
+            return Ok("Değer Başarıyla Silindi!");
+        
+        }
+        [HttpPut]
+        public IActionResult EditContact(UpdateContactDto updateContactDto)
+        {
+            var newValue = _mapper.Map<Contact>(updateContactDto);  
+            _contactService.TUpdate(newValue);
+            return Ok("İşlem Başarılı!Güncelleme Sağlandı!");
+        }
+
+    }
+}
---
+++ SeaIsland.WebApi/Controllers/DiscountsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SeaIsland.BusinessLayer.Abstract;
+using SeaIsland.DtoLayer.DiscountDto;
+using SeaIsland.EntityLayer.Entities;
+
+namespace SeaIsland.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController(IDiscountService _discountService,IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDiscounts()
+        {
+            var values = _discountService.TGetlistAll();
+            return Ok(values);
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var value = _discountService.TGetById(id);
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CreateDiscount(CreateDiscountDto createDiscountDto)
+        {
+            var newValue = _mapper.Map<Discount>(createDiscountDto);        
+            _discountService.TAdd(newValue);    
+            return Ok($"Ürün Başarıyla Eklendi : {newValue.Title}");
+        }
+        [HttpDelete]
+        public IActionResult DeleteDiscount(int id)
+        {
+            var value = _discountService.TGetById(id);
+            _discountService.TDelete(value);
+            return Ok($"Ürün Başarıyla Silindi : {value.Title}");
+        }
+        [HttpPut]
+        public IActionResult EditDiscount(UpdateDiscountDto updateDiscountDto)
+        {
+            var newValue = _mapper.Map<Discount>(updateDiscountDto);
+            _discountService.TUpdate(newValue);
+            return Ok("Ürün Başarıyla Güncellendi.");
+        }
+    }
+}
