ca5e9f0f7cf7212d3e253b767eaf4f3cecd51101
UmutUgurB <75566895+UmutUgurB@users.noreply.github.com>
2025-04-29 21:31:18 +0300

    api consume

--- SeIsland.WebUI/Controllers/CategoryController.cs
+++ SeIsland.WebUI/Controllers/CategoryController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SeIsland.WebUI.Dtos.CategoryDtos;
 
 namespace SeIsland.WebUI.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController(IHttpClientFactory _httpClientFactory) : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5221/api/Category");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();   
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);  
+                return View(values);
+            }
             return View();
         }
     }
---
+++ SeIsland.WebUI/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SeIsland.WebUI.Dtos.CategoryDtos
+{
+	public class ResultCategoryDto
+	{
+		public int CategoryId { get; set; }
+		public string CategoryName { get; set; }
+		public string CategoryDescription { get; set; }
+		public bool IsCategoryActive { get; set; }
+	}
+}
