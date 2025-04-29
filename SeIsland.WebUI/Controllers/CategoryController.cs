3d7d33721166998aee456291b4b6e69a69006c1c
UmutUgurB <75566895+UmutUgurB@users.noreply.github.com>
2025-04-29 22:44:03 +0300

    AddCategory

--- SeIsland.WebUI/Controllers/CategoryController.cs
+++ SeIsland.WebUI/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SeIsland.WebUI.Dtos.CategoryDtos;
+using System.Text;
 
 namespace SeIsland.WebUI.Controllers
 {
@@ -18,5 +19,24 @@ namespace SeIsland.WebUI.Controllers
             }
             return View();
         }
-    }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+		[HttpPost]
+		public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+		{
+            createCategoryDto.IsCategoryActive = true;  
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);  
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5221/api/Category",stringContent);
+            if(responseMessage.IsSuccessStatusCode) 
+            { 
+                return RedirectToAction("Index");   
+            }
+			return View();
+		}
+	}
 }
---
+++ SeIsland.WebUI/Dtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SeIsland.WebUI.Dtos.CategoryDtos
+{
+	public class CreateCategoryDto
+	{
+		public int CategoryId { get; set; }
+		public string CategoryName { get; set; }
+		public string CategoryDescription { get; set; }
+		public bool IsCategoryActive { get; set; }
+	}
+}
